5482d87c88603cd5ca2704661271f0fb43ead375
Simon Péter <simonpetymeg@gmail.com>
2025-12-01 15:27:41 +0100

    mivan?

---
+++ algo/beadando/dijkstra_algoritmus_final.py
@@ -0,0 +1,69 @@
+class Graf:
+
+    def __init__(self, dict=None):
+        if dict == None:
+            dict = {}
+        self.dict = dict
+
+    def getEdge(self, directed=True):
+        edges = []
+        for i in self.dict:
+            for j in self.dict[i]:
+                if directed:
+                    edges.append((i, j))
+                elif (i, j)not in edges:
+                    edges.append((i, j))
+        return edges
+
+    def getVertices(self):
+        return list(self.dict.keys())
+
+
+    def Neighbor(self, node):
+        return self.dict.get(node)
+
+    def dijkstra(self, source, target):
+        not_visited = set(self.dict.keys())
+        distance = {v: float('inf') for v in self.dict}
+        distance[source] = 0
+        previous = {}
+
+        while not_visited:
+            current = min((v for v in not_visited), key=lambda v: distance[v])
+            if distance[current] == float('inf'):
+                break
+        
+
+            not_visited.remove(current)
+            for neighbor, w in self.dict.get(current, []):
+                alt = distance[current] + w
+                if alt < distance[neighbor]:
+                    distance[neighbor] = alt
+                    previous[neighbor] = current
+
+        targetw = distance[target]
+
+        return distance, previous, targetw
+
+
+
+
+
+szotar = {
+    "A": [("B", 6), ("C", 2), ("D", 4)],#0
+    "B": [("E", 2), ("F", 5), ("D", 6)],#1
+    "C": [("F", 1)],#2
+    "D": [("H", 3)],#3
+    "E": [],#4
+    "F": [("I", 5)],#5
+    "G": [("C", 7), ("H", 11)],#6
+    "H": [],#7
+    "I": [("G", 7)],#8
+}
+
+
+graf = Graf(szotar)
+distance, previous, targetw = graf.dijkstra("A", "G")
+print("Távolságok:", distance)
+print("Előző csúcsok:", previous)
+print("Legrövidebb út a célponthoz:",targetw)
